e104cc3b3bbaea78b260d051a0136b104052c3ac
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-11-06 10:32:21 -0700

    solution, tests, etc

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,12 +1,40 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  if (arr.length === 0) {
+    return []
+  }
+
+  const min = Math.min(...arr);
+  const index = arr.indexOf(min);
+  arr.splice(index, 1);
+
+  const newArr = selectionSortRecursive(arr);
+  newArr.unshift(min);
+
+  return newArr
+  //my solution formats the array very oddly, with extra brackets around every new recursive loop
+  //so i checked the solution and changed it to fix the formatting,
+  //since i could not for the life of me figure out a way to make mine work
+  //see below for my original solution
+  if (arr.length > 0) {
+    return [min, selectionSortRecursive(arr)];
+  }
+  else return min;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
-
+  
+  let longInput = [];
+  for (index = 0; index < 100; index++) {
+    longInput.push(Math.floor(10*Math.random()))
+  }
+  console.log("Long Array: ")
+  console.log(longInput)
+  console.log("sorted: ")
+  console.log(selectionSortRecursive(longInput))
   console.log("");
 }
 
